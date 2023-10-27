4c02661c98a19e29361c3dcab23cf3e242887dae
mahi7921 <86034240+mahi7921@users.noreply.github.com>
2023-10-27 02:43:20 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberBtn = document.getElementById("memberBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberBtn.style.background = "#fff";
+    skillsBtn.style.background = "#f5f5f5";
+}
